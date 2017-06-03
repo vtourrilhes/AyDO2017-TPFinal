2265b3f483b4dd813d1c874cba386f52242be76b
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-03 20:46:27 -0300

    gestor calendario test

--- TpFinal/spec/gestorcalendario_spec.rb
+++ TpFinal/spec/gestorcalendario_spec.rb
@@ -7,16 +7,13 @@ describe 'GestorCalendario' do
 	before do
       @gestorCalendario = GestorCalendario.new
       @calendario = @gestorCalendario.crearCalendario('Laboral')
-      @respuestaLaboral = @calendario.getNombre
     end
 
   describe "OK" do
-    it "Si creo un calendario de nombre Laboral tengo que obtenerlo" do
-        expect(@respuestaLaboral).to eq 'Laboral'
-    end
     
     it "Si creo un calendario de nombre Laboral tengo que obtenerlo" do
-        expect(@gestorCalendario.obtenerCalendario('Laboral').getNombre).to eq 'Laboral'
+       calendario = @gestorCalendario.obtenerCalendario('Laboral')
+        expect(calendario.getNombre).to eq 'Laboral'
     end
     
    end
