607c59f17eb6723d2653a71a9df1eddca79cdaad
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-03 19:38:29 -0300

    calendario_spec

--- TpFinal/spec/calendario_spec.rb
+++ TpFinal/spec/calendario_spec.rb
@@ -4,13 +4,14 @@ require_relative '../model/Calendario'
 describe 'Calendario' do
 
 	before do
-      @calendario = Calendario.new("Laboral")
+      @calendario = Calendario.new('Laboral')
       @respuestaLaboral = @calendario.getNombre('Laboral')
     end
 
   describe "OK" do
     it "Si creo un calendario de nombre Laboral tengo que obtenerlo" do
         expect(@respuestaLaboral).to eq 'Laboral'
-      end
     end
+   end
+  
 end
