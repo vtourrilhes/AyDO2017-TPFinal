dc3c95109e159643104403b6e3fd7b69ccb05d9c
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-03 21:36:53 -0300

    gestor calendario test

--- TpFinal/model/gestorcalendario.rb
+++ TpFinal/model/gestorcalendario.rb
@@ -2,15 +2,18 @@ require_relative '../model/calendario'
 
 class GestorCalendario
 
-  calendarios = {}
+  def initialize()  
+    @calendarios = {}
+  end
+  
 
   def crearCalendario(nombreCalendario)
-    calendarios[nombreCalendario] = Calendario.new(nombreCalendario)
-    return calendarios[nombreCalendario]
+    @calendarios[nombreCalendario] = Calendario.new(nombreCalendario)
+    return @calendarios[nombreCalendario]
   end
   
   def obtenerCalendario(nombreCalendario)
-    return calendarios[nombreCalendario]
+    return @calendarios[nombreCalendario]
   end
 
 end
