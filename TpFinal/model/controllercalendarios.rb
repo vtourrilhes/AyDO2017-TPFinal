bf016c07eb8047d1f37903770c5da103a1dea4f9
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-12 20:42:41 -0300

    frecuencias

--- TpFinal/model/controllercalendarios.rb
+++ TpFinal/model/controllercalendarios.rb
@@ -14,6 +14,7 @@ class ControllerCalendarios
   attr_accessor :validadorDeJson
   attr_accessor :validadorCalendario
   attr_accessor :validadorEvento
+  attr_accessor :frecuencias
 
   def initialize()  
     self.repositoriocalendarios = RepositorioCalendarios.new    
