dd06a18fbb34ede258d612c5ddbe6d354f345f86
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-11 22:41:02 -0300

    app.rb

--- TpFinal/app.rb
+++ TpFinal/app.rb
@@ -17,23 +17,23 @@ post '/calendarios' do
     halt 400, "400 Bad Request: " + ex.to_s
   end
 end
-=begin
+
 delete '/calendarios' do
   begin
     nombre = params[:nombre]
     controlador.eliminarCalendario(nombre)
-    status 200
-  rescue Exception
-    # No encontrado
-    status 404
+    halt 200, "Se ha eliminado el calendario con exito el calendario " + calendario.nombre
+  rescue Exception ex
+    halt 400, "404 Not Found: " + ex.to_s
   end
 end
-=end
+
 get '/calendarios' do
   begin
-    calendarios = controlador.obtenerCalendarios()  
-    body convertidorJson.obtenerArrayJsonCalendarios(calendarios).to_json
-    status 200
+    calendarios = controlador.obtenerCalendarios()
+    halt 200, convertidorJson.obtenerArrayJsonCalendarios(calendarios).to_json
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
   end
 end
 
@@ -41,60 +41,69 @@ get '/calendarios/:nombre' do
   begin
     nombre = params[:nombre]
     calendario = controlador.obtenerCalendario(nombre)
-    body convertidorJson.obtenerJsonCalendario(calendario).to_json
-    status 200
-  rescue Exception => e
-    body e.message
-    status 404
+    halt 200, convertidorJson.obtenerJsonCalendario(calendario).to_json
+  rescue Exception => ex
+    halt 404, "404 Not Found: " + ex.to_s
   end
 end
-=begin
+
 post '/eventos' do
   begin
     request.body.rewind
     datos_json = JSON.parse request.body.read
     controlador.crearEvento(datos_json)
-    status 201
-  rescue Exception => e
-    body e.message
-    status 400
+    halt 201, "Se ha creado el evento con exito"
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
   end
 end
 
 put '/eventos' do
+  begin
   request.body.rewind
   datos_json = JSON.parse request.body.read
   controlador.actualizarEvento(datos_json)
-  status 200
+  halt 200, "Se ha actualizado el evento con exito"
+  rescue Exception ex
+    halt 404, "404 Not Found: " + ex.to_s
+  end
 end
 
 delete '/eventos/:id' do
   begin
   id_evento = params[:id]
   controlador.eliminarEvento(id_evento)
-  status 200
-  rescue Exception => e
-    body e.message
-    status 400
+  halt 200, "Se ha eliminado el evento con exito"
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
   end
-end
-=end
-get '/eventos' do
-  eventos = controlador.obtenerTodosLosEventos()
-  body convertidorJson.obtenerArrayJsonEventos(eventos).to_json
-  status 200
 end
 
 get '/eventos' do
+  begin
+  eventos = controlador.obtenerTodosLosEventos()
+  halt 200, convertidorJson.obtenerArrayJsonEventos(eventos).to_json
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
+  end
+end
+
+get '/eventos' do
+  begin
   id_calendario = params[:calendario]
   eventos = controlador.obtenerEventos(id_calendario.downcase)
-  body convertidorJson.obtenerArrayJsonEventos(eventos).to_json
-  status 200
+  halt 200, convertidorJson.obtenerArrayJsonEventos(eventos).to_json
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
+  end
 end
 
 get '/eventos/:id' do
+  begin
   id_evento = params[:id]
   evento = controlador.obtenerEvento(id_evento.downcase)
-  body convertidorJson.obtenerJsonEvento(evento).to_json
-  status 200
+  halt 200, convertidorJson.obtenerJsonEvento(evento).to_json
+  rescue Exception => ex
+    halt 400, "400 Bad Request: " + ex.to_s
+end
 end
