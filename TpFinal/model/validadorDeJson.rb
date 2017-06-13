d5dad2212094bf9e7b0fc6cb71d5274cc519906e
gabrielc6636 <gabrielc6636@gmail.com>
2017-06-13 19:32:50 -0300

    validadorDeEvento_spec

--- TpFinal/model/validadorDeJson.rb
+++ TpFinal/model/validadorDeJson.rb
@@ -10,11 +10,6 @@ class ValidadorDeJSON
   def validar_parametros_evento(params)
     raise TypeError unless params.is_a? Hash
     
-    raise TypeError unless params[:calendario].is_a? String
-    raise TypeError unless params[:id].is_a? String
-    raise TypeError unless params[:nombre].is_a? String
-    raise TypeError unless params[:inicio].is_a? Time
-    raise TypeError unless params[:fin].is_a? Time
     raise TypeError unless params[:recurrencia].is_a? Hash
     
     raise TypeError unless !(params.has_key? :id)
