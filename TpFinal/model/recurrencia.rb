fa935438c15babd88903de7223844c00682d1c36
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2017-06-21 06:46:32 -0300

    Evento Recurrente test

--- TpFinal/model/recurrencia.rb
+++
@@ -1,9 +0,0 @@
-class Recurrencia
-  attr_accessor :fecha_fin
-  attr_accessor :frecuencia
-
-  def initialize(fecha_fin, frecuencia)
-    @fecha_fin = fecha_fin
-    @frecuencia = frecuencia
-  end
-end
